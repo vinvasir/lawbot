4ed6ca2a4564277da0618cca2f6320d00503e97c
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-03-12 19:24:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lawbot::Application.config.secret_key_base = '98bb22cbaca88459200f7a31bfbb3f1314ae3221ef349c737ac356a1dda8575e285e50a99e3b68f046c97e5270a6093472c42f3d117ee43b895def99432bf686'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lawbot::Application.config.session_store :cookie_store, key: '_lawbot_session'
