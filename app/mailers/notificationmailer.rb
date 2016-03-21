1ddf94e11f902f1443f4c19504ef1ecaf3379b4a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-03-21 00:18:38 +0000

    Adjusted the automated email mailer

--- app/mailers/notificationmailer.rb
+++ app/mailers/notificationmailer.rb
@@ -1,8 +1,10 @@
 class Notificationmailer < ActionMailer::Base
   default from: "no-reply@lawbotapp.com"
 
-  def comment_added
-    mail(to: "prat.vasireddy@gmail.com",
-            subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place=comment.place
+    @place_owner=@place.user
+    mail(to: @place_owner.email,
+            subject: "A comment has been added to #{@place.name}")
   end
 end
